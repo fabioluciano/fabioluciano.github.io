36654d435db22e792af664f4fc9cb41b97465a74
Fábio Luciano <fabio@naoimporta.com>
2019-03-11 18:02:55 -0300

    teste

--- python/github_client.py
+++ python/github_client.py
@@ -6,21 +6,23 @@ class GithubClient:
     self.github_instance = Github(token)
     self.result = []
 
-  def get_repositories_from_users_by_topic(self, users=[], topics = []):
+  def get_repositories_from_users_by_topic(self, users=[], topic = ''):
     for user in users:
       github_user = self.github_instance.get_user(user)
 
       for repository in github_user.get_repos():
-        temp_repository = {}
-        temp_repository.update({ 'name': repository.name })
-        temp_repository.update({ 'html_url': repository.html_url })
-        temp_repository.update({ 'description': repository.description })
-        temp_repository.update({ 'homepage': repository.homepage })
-        self.result.append(temp_repository)
+        if topic in repository.get_topics() :
+          temp_repository = {}
+          temp_repository.update({ 'name': repository.name })
+          temp_repository.update({ 'html_url': repository.html_url })
+          temp_repository.update({ 'description': repository.description })
+          temp_repository.update({ 'homepage': repository.homepage })
+          self.result.append(temp_repository)
 
     print(self.result)
 
 
-github_client = GithubClient('')
+github_client = GithubClient('49ed5ccecf4ca18f37df9dfd3767cda82205cfa2')
 
-github_client.get_repositories_from_users_by_topic(users=['fabioluciano', 'integr8', 'utils-docker'])
+# github_client.get_repositories_from_users_by_topic(users=['fabioluciano', 'integr8', 'utils-docker'], topic='docker')
+github_client.get_repositories_from_users_by_topic(users=['fabioluciano', 'integr8', 'utils-docker'], topic='ansible')
