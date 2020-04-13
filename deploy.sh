9d2d8975c634d384917d9688445886aa0d885c00
Fábio Luciano <fabio@naoimporta.com>
2020-04-13 16:33:05 -0300

    Adding tekton pipelines

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 # Deploy Tekton Core 
 kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
+# Deploy Tekton Triggers
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+
 # Deploy Tekton Dashboard
 kubectl apply -f https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml
 
