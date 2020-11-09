e68192f37e1124dfc35c9f9770cdadb2e330efee
Fábio Luciano <fabio@naoimporta.com>
2020-11-08 23:27:47 -0300

    feat: adding picpay experience

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,3 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers
 
 # Deploy Tekton Dashboard
 kubectl apply -f https://github.com/tektoncd/dashboard/releases/latest/download/tekton-dashboard-release.yaml
-
-# Deploy Kaniko Task, to build the image
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/kaniko/kaniko.yaml
